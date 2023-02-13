9abb22d23d0bdb04a10225d0a192b6832fa48441
Cian747 <studiokaiser1@gmail.com>
2023-02-13 19:04:00 +0300

    made first migration

--- silo/models.py
+++ silo/models.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=50,blank=True,null=True)
     profile_photo = CloudinaryField('image',blank=True,null=True)
     personal_email = models.EmailField(blank=True,null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -24,7 +25,6 @@ class Profile(models.Model):
     # @classmethod
     # def search_username(cls,search_term):
     #     return cls.objects.filter(user__username__icontains = search_term)
-
 
     # def get_absolute_url(self):
     #     return reverse('profile',args=[str(self.id)])
--- silo/views.py
+++ silo/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth import authenticate,login,logout
+from .forms import CoachRegistrationForm
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
-    return render(request,'registration/signup.html')
+    return render(request,'index.html')
+
+def signin_user(request):
+    return render(request,'registration/login.html')
+
+def register(request):
+    rgf = CoachRegistrationForm()
+    if request.method == 'POST':
+        rgf = CoachRegistrationForm(request.POST)
+        if rgf.is_valid():
+            print(rgf)
+            rgf.save()
+            user = rgf.cleaned_data.get('username')
+            email = rgf.cleaned_data.get('email')
+            # send_welcome_email(user,email)
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+    
+    return render(request, 'registration/signup.html', {'rgf': rgf})
+
+def login_user(request):
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = password)
+        print(user)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+            
+
+    return render(request, 'registration/login.html')
