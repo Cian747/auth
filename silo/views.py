baf58c9887a776d0187188eef2909c4ac1c2d2e6
Cian747 <studiokaiser1@gmail.com>
2023-02-15 14:49:32 +0300

    prep for deployment

--- silo/views.py
+++ silo/views.py
@@ -29,7 +29,9 @@ def login_user(request):
     if request.method == 'POST':
 
         username = request.POST.get('username')
+        print(username)
         password = request.POST.get('password')
+        print(password)
 
         user = authenticate(request, username = username, password = password)
         print(user)
